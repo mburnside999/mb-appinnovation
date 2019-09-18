cf05f56eabfaeb50fb2d0673b5ba3a50d8cb382e
mburnside <mburnside@salesforce.com>
2019-09-18 10:40:14 +1000

    separate view for sfdb

--- index.js
+++ index.js
@@ -52,12 +52,12 @@ app.get('/db', function (request, response) {
 
 app.get('/sfdb', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
-    client.query('SELECT * FROM salesforce.account', function(err, result) {
+    client.query('SELECT id.name,type FROM salesforce.account', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.render('pages/sfdb', {results: result.rows} ); }
     });
   });
 });
