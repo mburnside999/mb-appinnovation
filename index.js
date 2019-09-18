13b70a78b31c13cd32a6c383ebaf0ddc90b2f5ac
mburnside <mburnside@salesforce.com>
2019-09-18 10:27:22 +1000

    added sfdb method

--- index.js
+++ index.js
@@ -49,6 +49,19 @@ app.get('/db', function (request, response) {
   });
 });
 
+
+app.get('/sfdb', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
+    client.query('SELECT * FROM salesforce.account', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 app.get('/rockets', function (req, res, next) {
   throw new Error('Kaboom!');
 });
